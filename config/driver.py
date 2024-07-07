9212fa548f0c8c211a3a4e126855acb6534345d0
Yevhen <galitsyn.evgeniy955@gmail.com>
2024-07-07 20:35:01 +0300

    first test on the git actions

--- config/driver.py
+++ config/driver.py
@@ -1,17 +1,15 @@
-import os
 from typing import Type, Dict
 
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
 
-from config import env
 from config.env import BROWSER
 
-EXPORT_RESULT_IN_TESTRAIL = env.get("EXPORT_RESULT_IN_TESTRAIL", True)
-RUN_ID = env.get("PLAN_ID", 0)
-CREATE_ALLURE_REPORT = env.get("CREATE_ALLURE_REPORT", True)
-ALLURE_REPORT_PATH = env.get("ALLURE_REPORT_PATH", os.path.dirname(__file__))
-SESSION_RESULT_PATH = env.get("SESSION_RESULT_PATH", os.path.dirname(__file__))
+# EXPORT_RESULT_IN_TESTRAIL = env.get("EXPORT_RESULT_IN_TESTRAIL", True)
+# RUN_ID = env.get("PLAN_ID", 0)
+# CREATE_ALLURE_REPORT = env.get("CREATE_ALLURE_REPORT", True)
+# ALLURE_REPORT_PATH = env.get("ALLURE_REPORT_PATH", os.path.dirname(__file__))
+# SESSION_RESULT_PATH = env.get("SESSION_RESULT_PATH", os.path.dirname(__file__))
 
 browsers: Dict[str, Type[WebDriver]] = {
     "chrome": webdriver.Chrome,
@@ -35,7 +33,7 @@ class Driver:
 
         if BROWSER == "chrome":
             self.options.add_argument("--window-size=1920,1080")
-            # self.options.add_argument("--headless")
+            self.options.add_argument("--headless")
 
         elif BROWSER in ["edge", "firefox"]:
             self.options.add_argument("--start-maximized")
