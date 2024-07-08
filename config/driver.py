3939cb9fc0391f403b5d6d909f1584fffb2d78fb
Yevhen <galitsyn.evgeniy955@gmail.com>
2024-07-08 09:46:59 +0300

    remote launch

--- config/driver.py
+++ config/driver.py
@@ -45,7 +45,7 @@ class Driver:
             self.options.add_argument("--no-sandbox")
             self.options.add_argument("--disable-dev-shm-usage")
             self.options.add_argument("--headless")
-            self.remote_url = "http://selenium:4444/wd/hub"
+            # self.remote_url = "http://selenium:4444/wd/hub"
 
     def start(self):
         if BROWSER in ["chrome", "edge", "firefox"]:
