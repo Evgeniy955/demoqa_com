ac875f6165177e8d1f123435e869f322a2c4dba9
Yevhen <galitsyn.evgeniy955@gmail.com>
2024-07-08 10:01:24 +0300

    remote launch

--- config/env.py
+++ config/env.py
@@ -11,4 +11,4 @@ def get(key, default=None):
     return var
 
 
-BROWSER = get("BROWSER", "firefox")  # browser name ["chrome" "edge", "firefox", "remote"]
+BROWSER = get("BROWSER", "chrome")  # browser name ["chrome" "edge", "firefox", "remote"]
