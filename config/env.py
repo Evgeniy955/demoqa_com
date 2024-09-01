d146237b1a483b896bd0aa88cec49ad7d38ad7d7
Yevhen335 <y.halitsyn@elsevier.com>
2024-09-01 21:01:14 +0300

    safari +

--- config/env.py
+++ config/env.py
@@ -11,6 +11,6 @@ def get(key, default=None):
     return var
 
 
-BROWSER: bool = get('BROWSER', 'chrome')  # browser name ["chrome", "edge", "firefox"]
-CREATE_ALLURE_REPORT: bool = get('CREATE_ALLURE_REPORT', False)
-SEND_REPORT: bool = get('SEND_REPORT', False)
+BROWSER = get('BROWSER', 'safari')  # browser name ["chrome", "edge", "firefox"]
+CREATE_ALLURE_REPORT = get('CREATE_ALLURE_REPORT', False)
+SEND_REPORT = get('SEND_REPORT', False)
